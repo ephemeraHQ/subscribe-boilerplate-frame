eb61c90e4b84dc1b4dba76c0f0bd5b9a55c7d605
daria-github <daria@xmtp.com>
2024-05-31 16:52:49 -0700

    add wasm check

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,37 @@
-/** @type {import('next').NextConfig} */
+import CopyPlugin from "copy-webpack-plugin";
 
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  async redirects() {
-    return [
-      {
-        source: "/transactionFrames",
-        destination: "https://www.xmtp.org/docs/tutorials/transaction-frames",
-        permanent: false,
-      },
-    ];
+  webpack: (config, { isServer, dev }) => {
+    config.externals.push("pino-pretty", "lokijs", "encoding");
+    if (isServer) {
+      if (!dev) {
+        config.plugins.push(
+          new CopyPlugin({
+            patterns: [
+              {
+                to: "./app/[name][ext]",
+                from: "node_modules/@xmtp/user-preferences-bindings-wasm/dist/node",
+                filter: (resourcePath) => resourcePath.endsWith(".wasm"),
+              },
+            ],
+          })
+        );
+      } else {
+        config.plugins.push(
+          new CopyPlugin({
+            patterns: [
+              {
+                to: "./vendor-chunks/[name][ext]",
+                from: "node_modules/@xmtp/user-preferences-bindings-wasm/dist/node",
+                filter: (resourcePath) => resourcePath.endsWith(".wasm"),
+              },
+            ],
+          })
+        );
+      }
+    }
+    return config;
   },
 };
 
