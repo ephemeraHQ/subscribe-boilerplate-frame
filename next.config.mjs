6700261de67839c71577738fe8520cfeee3af672
daria-github <daria@xmtp.com>
2024-06-01 14:40:37 -0700

    remove server check in config

--- next.config.mjs
+++ next.config.mjs
@@ -3,45 +3,43 @@ import path from "path";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config, { isServer }) => {
+  webpack: (config) => {
     config.externals = config.externals || [];
     config.externals.push("pino-pretty", "lokijs", "encoding");
     config.experiments = { ...config.experiments, topLevelAwait: true };
 
-    if (isServer) {
-      config.plugins.push(
-        new CopyPlugin({
-          patterns: [
-            {
-              from: path.resolve(
-                "node_modules/@xmtp/user-preferences-bindings-wasm/dist/node/*.wasm"
-              ),
-              to: path.resolve(".next/server/vendor-chunks/[name][ext]"),
-            },
-            {
-              from: path.resolve(
-                "node_modules/@xmtp/user-preferences-bindings-wasm/dist/node/*.wasm"
-              ),
-              to: path.resolve(".next/server/chunks/[name][ext]"),
-            },
-            {
-              from: path.resolve(
-                "node_modules/@xmtp/user-preferences-bindings-wasm/dist/node/*.wasm"
-              ),
-              to: path.resolve(".next/server/app/api/transaction/[name][ext]"),
-            },
-            {
-              from: path.resolve(
-                "node_modules/@xmtp/user-preferences-bindings-wasm/dist/node/*.wasm"
-              ),
-              to: path.resolve(
-                ".next/server/app/api/transaction-success/[name][ext]"
-              ),
-            },
-          ],
-        })
-      );
-    }
+    config.plugins.push(
+      new CopyPlugin({
+        patterns: [
+          {
+            from: path.resolve(
+              "node_modules/@xmtp/user-preferences-bindings-wasm/dist/node/*.wasm"
+            ),
+            to: path.resolve(".next/server/vendor-chunks/[name][ext]"),
+          },
+          {
+            from: path.resolve(
+              "node_modules/@xmtp/user-preferences-bindings-wasm/dist/node/*.wasm"
+            ),
+            to: path.resolve(".next/server/chunks/[name][ext]"),
+          },
+          {
+            from: path.resolve(
+              "node_modules/@xmtp/user-preferences-bindings-wasm/dist/node/*.wasm"
+            ),
+            to: path.resolve(".next/server/app/api/transaction/[name][ext]"),
+          },
+          {
+            from: path.resolve(
+              "node_modules/@xmtp/user-preferences-bindings-wasm/dist/node/*.wasm"
+            ),
+            to: path.resolve(
+              ".next/server/app/api/transaction-success/[name][ext]"
+            ),
+          },
+        ],
+      })
+    );
 
     return config;
   },
