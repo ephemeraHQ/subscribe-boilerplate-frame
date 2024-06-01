1a78f0255df9be81738e358d4f8e8ccabf101c28
daria-github <daria@xmtp.com>
2024-06-01 15:43:00 -0700

    move resolve to join

--- next.config.mjs
+++ next.config.mjs
@@ -6,44 +6,42 @@ const nextConfig = {
   webpack: (config) => {
     config.externals = config.externals || [];
     config.externals.push("pino-pretty", "lokijs", "encoding");
-    config.experiments = { ...config.experiments, topLevelAwait: true };
-
     config.plugins.push(
       new CopyPlugin({
         patterns: [
           {
-            from: path.resolve(
+            from: path.join(
               process.cwd(),
               "node_modules/@xmtp/user-preferences-bindings-wasm/dist/node/*.wasm"
             ),
-            to: path.resolve(
+            to: path.join(
               process.cwd(),
               ".next/server/vendor-chunks/[name][ext]"
             ),
           },
           {
-            from: path.resolve(
+            from: path.join(
               process.cwd(),
               "node_modules/@xmtp/user-preferences-bindings-wasm/dist/node/*.wasm"
             ),
             to: path.resolve(process.cwd(), ".next/server/chunks/[name][ext]"),
           },
           {
-            from: path.resolve(
+            from: path.join(
               process.cwd(),
               "node_modules/@xmtp/user-preferences-bindings-wasm/dist/node/*.wasm"
             ),
-            to: path.resolve(
+            to: path.join(
               process.cwd(),
               ".next/server/app/api/transaction/[name][ext]"
             ),
           },
           {
-            from: path.resolve(
+            from: path.join(
               process.cwd(),
               "node_modules/@xmtp/user-preferences-bindings-wasm/dist/node/*.wasm"
             ),
-            to: path.resolve(
+            to: path.join(
               process.cwd(),
               ".next/server/app/api/transaction-success/[name][ext]"
             ),
