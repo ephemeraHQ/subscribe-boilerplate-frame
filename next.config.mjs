f23ae698db5cac538e377c1d7bc9c1afc7f9def2
daria-github <daria@xmtp.com>
2024-06-03 07:25:26 -0700

    remove webpack from next config

--- next.config.mjs
+++ next.config.mjs
@@ -6,55 +6,6 @@ const nextConfig = {
   experimental: {
     serverComponentsExternalPackages: ["@xmtp/user-preferences-bindings-wasm"],
   },
-  webpack: (config) => {
-    config.externals = config.externals || [];
-    config.externals.push("pino-pretty", "lokijs", "encoding");
-    config.plugins.push(
-      new CopyPlugin({
-        patterns: [
-          {
-            from: path.join(
-              process.cwd(),
-              "node_modules/@xmtp/user-preferences-bindings-wasm/dist/node/*.wasm"
-            ),
-            to: path.join(
-              process.cwd(),
-              ".next/server/vendor-chunks/[name][ext]"
-            ),
-          },
-          {
-            from: path.join(
-              process.cwd(),
-              "node_modules/@xmtp/user-preferences-bindings-wasm/dist/node/*.wasm"
-            ),
-            to: path.resolve(process.cwd(), ".next/server/chunks/[name][ext]"),
-          },
-          {
-            from: path.join(
-              process.cwd(),
-              "node_modules/@xmtp/user-preferences-bindings-wasm/dist/node/*.wasm"
-            ),
-            to: path.join(
-              process.cwd(),
-              ".next/server/app/api/transaction/[name][ext]"
-            ),
-          },
-          {
-            from: path.join(
-              process.cwd(),
-              "node_modules/@xmtp/user-preferences-bindings-wasm/dist/node/*.wasm"
-            ),
-            to: path.join(
-              process.cwd(),
-              ".next/server/app/api/transaction-success/[name][ext]"
-            ),
-          },
-        ],
-      })
-    );
-
-    return config;
-  },
 };
 
 export default nextConfig;
